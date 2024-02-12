14fc365f20576e86d1fda557952c53632c40717a
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2024-02-12 20:10:32 +0100

    more shift

--- keyboards/splitkb/aurora/helix/keymaps/robbplo/keymap.c
+++ keyboards/splitkb/aurora/helix/keymaps/robbplo/keymap.c
@@ -10,8 +10,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,         KC_5,                     KC_6,    KC_7,          KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,         KC_B,                     KC_J,    KC_L,          KC_U,    KC_Y,    KC_SCLN, KC_DEL,
         KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,         KC_G,                     KC_M,    KC_N,          KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,         KC_V,   KC_MPLY, KC_MUTE, KC_K,    KC_H,          KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-        MO(3),   KC_GRV,  KC_LALT, KC_LGUI, KC_LALT,      KC_SPC, MO(1),   MO(2),   KC_SPC,   LALT(KC_GRV), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,         KC_V,   KC_MPLY, KC_MUTE, KC_K,    KC_H,          KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
+        MO(3),   KC_GRV,  KC_LALT, KC_LGUI, KC_LALT,      KC_SPC, MO(1),   MO(2),   KC_ENT,  LALT(KC_GRV),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 	[1] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,

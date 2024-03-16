1a588a8fb2968241c0c8a67fc7fd2e211d931641
Robbin Ploeger <robbplo@gmail.com>
2024-03-16 12:27:50 +0100

    add sine wave to oled

--- keyboards/splitkb/aurora/helix/helix.c
+++ keyboards/splitkb/aurora/helix/helix.c
@@ -1,4 +1,5 @@
 /* Copyright 2023 splitkb.com <support@splitkb.com>
+ * p
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +16,8 @@
  */
 
 #include "quantum.h"
+#include <math.h>
+#include <stdlib.h>
 
 #ifdef OLED_ENABLE
 // NOTE: Most of the OLED code was originally written by Soundmonster for the Corne,
@@ -24,105 +27,49 @@ oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
     return OLED_ROTATION_270;
 }
 
-void render_mod_status_gui_alt(uint8_t modifiers) {
+#define OLED_H 128
+#define OLED_W 32
 
-}
 
-void render_mod_status_ctrl_shift(uint8_t modifiers) {
-    static const char PROGMEM ctrl_off_1[] = {0x89, 0x8a, 0};
-    static const char PROGMEM ctrl_off_2[] = {0xa9, 0xaa, 0};
-    static const char PROGMEM ctrl_on_1[] = {0x91, 0x92, 0};
-    static const char PROGMEM ctrl_on_2[] = {0xb1, 0xb2, 0};
-
-    static const char PROGMEM shift_off_1[] = {0x8b, 0x8c, 0};
-    static const char PROGMEM shift_off_2[] = {0xab, 0xac, 0};
-    static const char PROGMEM shift_on_1[] = {0xcd, 0xce, 0};
-    static const char PROGMEM shift_on_2[] = {0xcf, 0xd0, 0};
-
-    // fillers between the modifier icons bleed into the icon frames
-    static const char PROGMEM off_off_1[] = {0xc5, 0};
-    static const char PROGMEM off_off_2[] = {0xc6, 0};
-    static const char PROGMEM on_off_1[] = {0xc7, 0};
-    static const char PROGMEM on_off_2[] = {0xc8, 0};
-    static const char PROGMEM off_on_1[] = {0xc9, 0};
-    static const char PROGMEM off_on_2[] = {0xca, 0};
-    static const char PROGMEM on_on_1[] = {0xcb, 0};
-    static const char PROGMEM on_on_2[] = {0xcc, 0};
-
-    if(modifiers & MOD_MASK_CTRL) {
-        oled_write_P(ctrl_on_1, false);
-    } else {
-        oled_write_P(ctrl_off_1, false);
+void render_sine(bool invert) {
+    double pad = 1;
+    double prev = (1 - cos(2 * M_PI * (OLED_H - 1) / OLED_W - 1)) * 16;
+    if (invert) {
+        prev = OLED_W - prev;
     }
 
-    if ((modifiers & MOD_MASK_CTRL) && (modifiers & MOD_MASK_SHIFT)) {
-        oled_write_P(on_on_1, false);
-    } else if(modifiers & MOD_MASK_CTRL) {
-        oled_write_P(on_off_1, false);
-    } else if(modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(off_on_1, false);
-    } else {
-        oled_write_P(off_off_1, false);
+    for (uint8_t y = 0; y < OLED_H; y++) {
+        double x = (1 - cos(2 * M_PI * y / OLED_W - 1)) * ((OLED_W - pad * 2) / 2) + pad;
+
+        if (invert) {
+            x = OLED_W - x;
+        }
+
+        if (x - prev >= 0) {
+            for (uint8_t xs = prev - pad; xs <= x + pad; xs++) {
+                oled_write_pixel(xs, y, true);
+            }
+        } else {
+            for (uint8_t xs = x - pad; xs <= prev + pad; xs++) {
+                oled_write_pixel(xs, y, true);
+            }
+        }
+
+        prev = x;
+
     }
-
-    if(modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(shift_on_1, false);
-    } else {
-        oled_write_P(shift_off_1, false);
-    }
-
-    if(modifiers & MOD_MASK_CTRL) {
-        oled_write_P(ctrl_on_2, false);
-    } else {
-        oled_write_P(ctrl_off_2, false);
-    }
-
-    if (modifiers & MOD_MASK_CTRL & MOD_MASK_SHIFT) {
-        oled_write_P(on_on_2, false);
-    } else if(modifiers & MOD_MASK_CTRL) {
-        oled_write_P(on_off_2, false);
-    } else if(modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(off_on_2, false);
-    } else {
-        oled_write_P(off_off_2, false);
-    }
-
-    if(modifiers & MOD_MASK_SHIFT) {
-        oled_write_P(shift_on_2, false);
-    } else {
-        oled_write_P(shift_off_2, false);
-    }
-}
-
-void render_logo(void) {
-    static const char PROGMEM aurora_logo[] = {
-        0x00, 0x00, 0x00, 0xe0, 0x00, 0xf8, 0xc0, 0xf8, 0xe0, 0xc0, 0xfc, 0x00, 0x7e, 0x18, 0x00, 0x80,
-        0x00, 0x02, 0x80, 0xf0, 0x00, 0xc0, 0x80, 0xf8, 0xc0, 0xe0, 0x70, 0x60, 0x3c, 0x38, 0x3c, 0x1c,
-        0x00, 0x3f, 0x0c, 0x0f, 0x1f, 0x03, 0x07, 0x01, 0xc3, 0x00, 0xe0, 0x80, 0x00, 0xe0, 0x80, 0xf8,
-        0x80, 0xc0, 0xf7, 0xc7, 0x6f, 0x7b, 0x39, 0x30, 0x00, 0x80, 0x00, 0xc0, 0x00, 0xc0, 0xc2, 0xe0,
-        0x00, 0x40, 0x38, 0x30, 0x38, 0x1e, 0x18, 0x1e, 0x0f, 0x0c, 0x07, 0x07, 0x07, 0x03, 0x03, 0x21,
-        0x21, 0x31, 0x30, 0x18, 0x18, 0x1c, 0x08, 0x0c, 0x0e, 0x07, 0x06, 0x07, 0x03, 0xc3, 0x03, 0x01,
-        0x4c, 0xcc, 0xc2, 0xc2, 0x41, 0x49, 0x09, 0x2b, 0x2a, 0x6a, 0x6e, 0x24, 0x24, 0x04, 0x92, 0x92,
-        0xb1, 0xf1, 0xf1, 0xf2, 0xe6, 0xa4, 0xa4, 0x04, 0x04, 0x28, 0x28, 0x28, 0x28, 0x28, 0x28, 0x28
-    };
-    oled_write_raw_P(aurora_logo, sizeof(aurora_logo));
-    oled_set_cursor(0, 4);
-}
-
-void render_kb_LED_state(void) {
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("N ") : PSTR("  "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("C ") : PSTR("  "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("S ") : PSTR("  "), false);
-}
-
-void render_layer_state(void) {
-
 }
 
 
 bool oled_task_kb(void) {
+    if (is_keyboard_master()) {
+        render_sine(false);
+    } else {
+        render_sine(true);
+    }
+
+    oled_scroll_left();
+
     return false;
 }
 #endif
@@ -152,3 +99,4 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     return true;
 }
 #endif
+
